d20da7dac360986de2121d774f3955994a826dbc
Anshul Thakur <66345681+dr-ghost@users.noreply.github.com>
2022-12-21 16:07:16 +0000

    Co-authored-by: nikhil96134@gmail.com <nikhil96134@gmail.com> Co-authored-by: saksham08jain <saksham08jain@users.noreply.github.com>

--- python/pyscrpt.py
+++ python/pyscrpt.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from datetime import time
+from itertools import combinations
 
 import queue
 
@@ -8,6 +9,14 @@ node_lst = []
 drone_type_instances = []
 successful_drone_type_instances = []
 const_time = 4 * 60 * 60
+
+#precomp
+item_lst = []
+item_weights = [1,6,4,2,5]
+item_lengths = [5,5,5,15,20]
+item_breadths = [8,10,10,10,15]
+item_heights = [5,8,15,8,10]
+item_volumes = [item_lengths[i]*item_breadths[i]*item_heights[i] for i in range(len(item_lengths))]
 
 class Point:
     def __init__(self,X,Y,Z):
@@ -41,9 +50,11 @@ class Warehouse(Node):
     def __init__(self,stationID,charging_current,X,Y):
         self.ID = stationID
         self.cc = charging_current
+    
     def __str__(self):
         return "{ID}".format(ID = self.ID)
 
+global_st = Warehouse("ST",0,0,0)
 
 class RechargeStation(Node):
   def __init__(self):
@@ -51,13 +62,15 @@ class RechargeStation(Node):
 
 
 class Item:
-  def __init__(self,ItemId,Weight,Length,Breadth,Height):
-    self.ItemId=ItemId
-    self.Weight=Weight
-    self.Length=Length
-    self.Breadth=Breadth
-    self.Height=Height
-    self.volume = Length*Breadth*Height
+    def __init__(self,ItemId,DemandID):
+       self.ItemId=ItemId
+       self.weight = item_weights[int(ItemId[-1])-1]
+       self.length = item_lengths[int(ItemId[-1])-1]
+       self.breadth = item_breadths[int(ItemId[-1])-1]
+       self.height = item_heights[int(ItemId[-1])-1]
+       self.volume = item_volumes[int(ItemId[-1])-1]
+       self.demand = DemandID
+       
 
 
 class Demand(Node):
@@ -79,7 +92,7 @@ class Demand(Node):
 
 
 class Drone:
-    def __init__(self, droneType:str, base_weight:float, weight_capacity:float,  battery_capacity:float, remaining_battery:float, payload_vol:float, num_slots:int, max_speed:float, time = 4 * 60 * 60, position = Point(0,0,0), recharge_energy = 0.0,a:float,b:float,c:float,p:float,q:float, path_summary = []):
+    def __init__(self, droneType:str, base_weight:float, weight_capacity:float,  battery_capacity:float, remaining_battery:float, payload_vol:float, num_slots:int, max_speed:float, a:float, b:float, c:float, p:float, q:float, time = 4 * 60 * 60, position = Point(0,0,0), recharge_energy = 0.0, path_summary = []):
         self.type = droneType
         self.battery_capacity = battery_capacity
         self.slots = num_slots
@@ -103,31 +116,37 @@ class Drone:
 
 
 class Path:
-    def __init__(self,p1:Point,p2:Point,drone:Drone):
+    def __init__(self,p1:Point,p2:Point,drone:Drone, mx_speed:tuple):
         self.drone = drone
+        self.mx_speed = mx_speed
     def get_path(self,p1:Point,p2:Point):
         lst = []
-        for i in range(p1.x, p2.x, self.drone.speed):
+        for i in range(p1.x, p2.x, self.mx_speed[0]):
             lst.append(Point(i,p1.y,p1.z))
-        for i in range(p1.y, p2.y, self.drone.speed):
+        for i in range(p1.y, p2.y, self.mx_speed[0]):
             lst.append(Point(p2.x,i,p1.z))
-        for i in range(p1.z, p2.z, self.drone.speed):
+        for i in range(p1.z, p2.z, self.mx_speed[1]):
             lst.append(Point(p2.x,p2.y,i))
         return lst
     def get_energy(self,p1:Point,p2:Point):
-        return self.drone.weight * (self.drone.a + self.drone.b * self.drone.speed + self.drone.c * p1.z)
+        t_z = 0.0
+        t_h = 0.0
+        for i in range(p1.z, p2.z, self.mx_speed[1]):
+            t_z += self.drone.weight * (self.drone.a + self.drone.b * self.mx_speed[1] + self.drone.c * t_h)
+            t_h += self.mx_speed[1]
+        return self.drone.weight * (self.drone.a + self.drone.b * self.mx_speed[0]) * (abs(p1.x - p2.x)/self.mx_speed[0]) + self.drone.weight * (self.drone.a + self.drone.b * self.mx_speed[0]) * (abs(p1.y - p2.y)/self.mx_speed[0]) + t_z
     def get_time(self,p1:Point,p2:Point):
-        return (p2.x - p1.x + p2.y - p1.y + p2.z - p1.z) / self.drone.speed
+        return abs(p1.x - p2.x)/self.mx_speed[0] + abs(p1.y - p2.y)/self.mx_speed[0] + abs(p1.z - p2.z)/self.mx_speed[1]
     def __str__(self):
         return "Path: {}\nEnergy: {}\nTime: {}".format(self.lst, self.energy, self.time)
 
-def node_path(drone:Drone, node:Node, r_time:float, battery:float, visited:dict = {i : -1 for i in node_lst if type(i) == Warehouse or type(i) == RechargeStation}, dest_visited:int = 0, path_summary:list = [], pkg_weight:float = 0.0, pkg_vol:float = 0.0):
+def node_path(drone:Drone, node:Node, r_time:float, battery:float, visited:dict = {i : -1 for i in node_lst if type(i) == Warehouse or type(i) == RechargeStation}, dest_visited:int = 0, path_summary:list = [], energy_consumed:float = 0.0):
     if type(node) == Warehouse:
-        successful_drone_type_instances.append((drone, r_time, battery, visited, dest_visited, path_summary))
+        successful_drone_type_instances.append((drone, r_time, visited, dest_visited, path_summary, energy_consumed))
     for i in node_lst:
         if type(i) == Warehouse:
            if visited[i] < dest_visited:
-                t_p = Path(node.position, i.position, drone)
+                t_p = Path(node.position, i.position, drone, )
                 if battery - t_p.get_energy(node.position, i.position) > 0 and r_time + t_p.get_time(node.position, i.position) < const_time:
                    node_path(drone, i, r_time + t_p.get_time(node.position, i.position), battery - t_p.get_energy(node.position, i.position), visited, dest_visited, path_summary + [t_p])
         elif type(i) == RechargeStation:
@@ -141,19 +160,25 @@ def node_path(drone:Drone, node:Node, r_time:float, battery:float, visited:dict 
                 if battery - t_p.get_energy(node.position, i.position) > 0 and r_time + t_p.get_time(node.position, i.position) < const_time:
                    node_path(drone, i, r_time + t_p.get_time(node.position, i.position), battery - t_p.get_energy(node.position, i.position), visited, dest_visited + 1, path_summary + [t_p], pkg_weight + i.item.weight, pkg_vol + i.item.volume)
 
-  
-def main():
-    global dem_lst
-    
-    Item1 = Item('Item-1',1,5,8,5)
-    Item2 = Item('Item-2',6,5,10,8)
-    Item3 = Item('Item-3',4,5,10,15)
-    Item4 = Item('Item-4',2,15,10,8)
-    Item5 = Item('Item-5',5,20,15,10)
-    
+def carry_lst_generator(r_weight, r_volume, item_lst:list):
+    lst = []
+    for i in item_lst:
+        if i.weight <= r_weight and i.volume <= r_volume:
+            lst.append([[i] + x for x in carry_lst_generator(r_weight - i.weight, r_volume - i.volume, [j for j in item_lst if i != j])])   
+    return lst
+
+def node_search(drone:Drone):
+    carry_lst = carry_lst_generator(drone.weight_capacity, drone.volume_capacity, item_lst)
+    for i in carry_lst:
+        node_path(drone, global_st, 0, drone.battery_capacity)
+
+
+
+
+def main():   
     demand = pd.read_csv("Demand.csv")
     parameter = pd.read_csv("Parameters.csv")
-    demands = []        #Demand List
+    demands = []        
     W1 = Warehouse('WH1',5,0,0)
     for i in range(len(demand['Demand ID'])):
         D = Demand(demand['Demand ID'][i],W1,demand['Item'][i],demand['Day'][i][-1],demand['X'][i],demand['Y'][i],demand['Z'][i],demand['DeliveryFrom'][i],demand['DeliveryTo'][i])
@@ -165,20 +190,20 @@ def main():
     p_values=[]
     q_values=[]
     
-    for i in range(len(parameter)):
-        if(parameter["Parameter_ID"][0]=="P"):
-            p_values.append(parameter["Value"][i])
-        if(parameter["Parameter_ID"][0]=="Q"):
-            q_values.append(parameter["Value"][i])
-        if(parameter["Parameter_ID"][0]=="A"):
-            a_values.append(parameter["Value"][i])
-        if(parameter["Parameter_ID"][0]=="B"):
-            b_values.append(parameter["Value"][i])
-        if(parameter["Parameter_ID"][0]=="C" and parameter["Parameter_ID"] !="Cost(C)"):
-            c_values.append(parameter["Value"][i])
-        if(parameter["Parameter_ID"]=="Cost(C)"):
+    for i in range(len(parameter["Value"])):
+        if(parameter["Parameter_ID"][i][0]=="P"):
+            p_values.append(float(parameter["Value"][i]))
+        if(parameter["Parameter_ID"][i][0]=="Q"):
+            q_values.append(float(parameter["Value"][i]))
+        if(parameter["Parameter_ID"][i][0]=="A"):
+            a_values.append(float(parameter["Value"][i]))
+        if(parameter["Parameter_ID"][i][0]=="B"):
+            b_values.append(float(parameter["Value"][i]))
+        if(parameter["Parameter_ID"][i][0]=="C" and parameter["Parameter_ID"][i] !="Cost(C)"):
+            c_values.append(float(parameter["Value"][i]))
+        if(parameter["Parameter_ID"][i]=="Cost(C)"):
             cost=parameter["Value"][i]
-        if(parameter["Parameter_ID"]=="MaxSpeed (M)"):
+        if(parameter["Parameter_ID"][i]=="MaxSpeed (M)"):
             max_speed=parameter["Value"][i]
         
         
